8ffb732cddf8ddc691248ed80e12d12834d57173
Daniel Kitheka <dankinyi99@gmail.com>
2024-02-29 11:47:20 +0300

    added service testing

--- Rhyde-NG/src/app/services/api.service.spec.ts
+++ Rhyde-NG/src/app/services/api.service.spec.ts
@@ -1,16 +1,78 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
+import { expectedUsers } from './testdata/users';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ApiService);
+    testingController = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('gets all users', ()=>{
+    service.getUsers().subscribe((users: any)=>{
+      expect(users).toBeTruthy()
+      expect(users.length).toBe(3)
+    })
+
+    const mockReq = testingController.expectOne('http://localhost:4100/users')
+    mockReq.flush(Object.values(expectedUsers))
+    expect(mockReq.request.method).toBe('GET')
+  })
+
+  it('gets user by id', ()=>{
+    let id = '206ff9b5-5413-4f20-b343-501285fcf0a3'
+    service.getOneUserDetails(id).subscribe((user:any)=>{
+      expect(user).toBeTruthy();
+      expect(user.name).toBe('Meshack Korir')
+    })
+
+    const mockReq = testingController.expectOne(`http://localhost:4100/users/${id}`)
+    mockReq.flush(expectedUsers[0])
+    expect(mockReq.request.method).toBe('GET')
+  })
+
+  it('registers a user', ()=>{
+    let mockUser = {
+      name: "user",
+      email: "user@yopmail.com",
+      phone_number: "0787543211",
+      role: "user",
+      password: "user",
+      location: "Nairobi",
+      profile_image: ''
+    }
+
+    service.registerUser(mockUser).subscribe(res=>{
+      expect(res.message).toEqual("User registered successfully")
+    })
+
+    const mockReq = testingController.expectOne('http://localhost:4100/users');
+    expect(mockReq.request.method).toEqual('POST')
+    expect(mockReq.request.body).toBe(mockUser)
+    mockReq.flush({"message": "User registered successfully"})
+  })
+
+  it('deletes a user', ()=>{
+    let id = '206ff9b5-5413-4f20-b343-501285fcf0a3'
+
+    service.deleteUser(id).subscribe((res:any)=>{
+      expect(res).toBeTruthy();
+      expect(res.message).toBe('Deleted successfully')
+    })
+
+    const mockReq = testingController.expectOne(`http://localhost:4100/users/delete/${id}`)
+    expect(mockReq.request.method).toBe('DELETE')
+  })
 });
---
+++ Rhyde-NG/src/app/services/testdata/users.ts
@@ -0,0 +1,38 @@
+export let expectedUsers = [
+    {
+      user_id: "206ff9b5-5413-4f20-b343-501285fcf0a3",
+      name: "Meshack Korir",
+      email: "meshackkorir100@gmail.com",
+      phone_number: "0198678330",
+      role: "customer",
+      Password: "$2b$05$vHyaKgSauV7NzwaF.3j6PeMfiUguMxCoVCxrExB.GELemN6K8TFPm",
+      profile_image: "https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_640.png",
+      location: "Nairobi",
+      isDeleted: false,
+      isWelcomed: true
+    },
+    {
+      user_id: "62df57e4-f2d7-4ddd-898c-022f91582113",
+      name: "James Kariuki",
+      email: "jamesew2605@gmail.com",
+      phone_number: "0798678330",
+      role: "driver",
+      Password: "$2b$05$BB8VD5zI2iqrXjOVBeonG.pD4y7r0rCc10tVmDkxFL05bHxjXgx6i",
+      profile_image: "https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_640.png",
+      location: "Nairobi",
+      isDeleted: false,
+      isWelcomed: true
+    },
+    {
+      user_id: "55875f2c-89c5-418f-a85c-13b14d04b24c",
+      name: "Godwin Maina",
+      email: "godwingathogo91@gmail.com",
+      phone_number: "0123456789",
+      role: "driver",
+      Password: "12345678",
+      profile_image: "https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_640.png",
+      location: "Nanyuki",
+      isDeleted: false,
+      isWelcomed: true
+    }
+]
